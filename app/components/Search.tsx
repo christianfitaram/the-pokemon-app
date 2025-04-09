71f8ad6424632a2774ad76c1114fb68a271a210a
christianfitaram <christianfitaram@gmail.com>
2025-04-09 21:04:58 +0200

    Fix: responsive styles

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -237,30 +237,30 @@ const Search: React.FC<ToDisplayProps> = ({
           </span>
         ))}
       </div>
-      <div className="flex flex-row justify-center gap-4">
+      <div className="grid grid-cols-2 sm:flex sm:flex-row justify-center gap-4 mt-4">
         <button
-          className="border p-2 rounded-lg text-white   hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
+          className="border p-2 rounded-lg text-white hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
           onClick={getRandomPokemon}
         >
-          Random pokemon
+          Random Pokémon
         </button>
         <button
-          className="border p-2 rounded-lg text-white   hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
+          className="border p-2 rounded-lg text-white hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
           onClick={toListRecentlyViewed}
         >
-          Recent viewed
+          Recently Viewed
         </button>
         <button
           onClick={showAssistantChat}
-          className="border p-2 text-white  rounded-lg   hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
+          className="border p-2 text-white rounded-lg hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
         >
-          Get AI help
+          Get AI Help
         </button>
         <button
           onClick={goToHome}
-          className="border p-2 text-white rounded-lg hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
+          className="border flex flex-row items-center justify-center gap-2 p-2 text-white rounded-lg hover:bg-gray-700 transition-transform duration-300 ease-in-out hover:scale-105"
         >
-          <FaHome className="h-5 w-5" />{" "}
+          <FaHome className="h-5 w-5" /> Go Home
         </button>
       </div>
     </div>
