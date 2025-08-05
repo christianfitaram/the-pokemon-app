97470bb048df5cebe0a2cd06dab63b9b3293d462
Christian Fita <c.fita@gindumac.com>
2025-08-05 08:17:32 +0200

    Refactor: Clean up formatting and improve readability in NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,28 @@
 "use client";
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 function NotFound() {
-    const router = useRouter();
-    return (
-        <div className="flex flex-col items-center space-y-6">
-            <h1 className='text-2xl'>It seems that you got lost!</h1>
-            <p className='text-2xl'>Error 404</p>
-            <Image src="../assets/img/psyduck.svg"
-                alt='not-found'
-                width={300} // Width of the image
-                height={300} // Height of the image
-            >
-            </Image>
-            <div>
-                <button onClick={() => router.push('/')} className="text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200  bg-blue-600   font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">
-                    Back to Home
-                </button>
-            </div>
-        </div>
-    )
+  const router = useRouter();
+  return (
+    <div className="flex flex-col items-center space-y-6">
+      <h1 className="text-2xl">It seems that you got lost!</h1>
+      <p className="text-2xl">Error 404</p>
+      <Image
+        src="../assets/img/psyduck.svg"
+        alt="not-found"
+        width={300} // Width of the image
+        height={300} // Height of the image
+      ></Image>
+      <div>
+        <button
+          onClick={() => router.push("/")}
+          className="text-white  bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+        >
+          Back to Home
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
