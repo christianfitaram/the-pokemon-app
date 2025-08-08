a4cc4317a8130b00e2bf0c81157452b182333729
Christian Fita <c.fita@gindumac.com>
2025-08-08 07:23:13 +0200

    Refactor `useAllPokemonNames` hook: Adjust formatting and code indentation for improved readability.

--- hooks/useAllPokemonNames.ts
+++ hooks/useAllPokemonNames.ts
@@ -1,34 +1,33 @@
-import { useEffect, useState } from "react";
-import { Pokemon } from "@/types/interfaces";
-import { PokemonApiClient } from "@/lib/api_clients/pokemonApiClient";
+import {useEffect, useState} from "react";
+import {Pokemon} from "@/types/interfaces";
+import {PokemonApiClient} from "@/lib/api_clients/pokemonApiClient";
 
 export function useAllPokemonNames() {
-  const [pokemonNames, setPokemonNames] = useState<Pokemon[]>([]);
-  const [loading, setLoading] = useState(true);
+    const [pokemonNames, setPokemonNames] = useState<Pokemon[]>([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const stored = localStorage.getItem("allPokemonNames");
-    if (stored) {
-      setPokemonNames(JSON.parse(stored));
-      setLoading(false);
-    } else {
-      PokemonApiClient.getAllPokemons()
-        .then((response) => {
-          // Assuming response.data contains the array of Pokemon
-          if (response.success && Array.isArray(response.data)) {
-            const pokemonData = response.data;
-            localStorage.setItem("allPokemonNames", JSON.stringify(pokemonData));
-            setPokemonNames(pokemonData);
-          } else {
-            console.error("Invalid response format:", response);
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching Pokemon names:", error);
-        })
-        .finally(() => setLoading(false));
-    }
-  }, []);
+    useEffect(() => {
+        const stored = localStorage.getItem("allPokemonNames");
+        if (stored) {
+            setPokemonNames(JSON.parse(stored));
+            setLoading(false);
+        } else {
+            PokemonApiClient.getAllPokemons()
+                .then((response) => {
+                    if (response.success && response.data) {
+                        const pokemonData =  response.data;
+                        localStorage.setItem("allPokemonNames", JSON.stringify(pokemonData));
+                        setPokemonNames(pokemonData);
+                    } else {
+                        console.error("Invalid response format:", response);
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error fetching Pokemon names:", error);
+                })
+                .finally(() => setLoading(false));
+        }
+    }, []);
 
-  return { pokemonNames, loading };
+    return {pokemonNames, loading};
 }
